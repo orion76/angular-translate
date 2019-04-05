a9a62e056277b32c2beb6c43d168f12e3c93bea4
orion76 <gpb@yandex.ru>
2019-04-05 06:02:14 +0000

    fix

--- src/app/app-http.module.ts
+++ src/app/app-http.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { HTTPAuthModule } from '@app-library/user/auth/module';
 import { OAuthPluginService } from '@app-library/user/auth/plugins/oauth.plugin';
 import { HTTPDataModule } from '@app-services/data/module';
-import { ResponseJsonapiConverter } from '@app-services/data/response/converters/jsonapi-converter.service';
+
 
 @NgModule({
   declarations: [
