24f4a88df922be82fc666c695a04e6028e39a61f
orion76 <gpb@yandex.ru>
2019-04-09 12:06:23 +0400

    work

--- src/app/libraries/common/entity/entity.ts
+++ src/app/libraries/common/entity/entity.ts
@@ -1,11 +1,28 @@
 import * as Immutable from 'immutable';
-import { IEntity } from './types';
+import {IEntity} from './types';
+import {TEntityFields} from '@app-library/components/form/types';
 
 
-export function createEntity<T extends IEntity>(source: string, id: string, fields = {}) {
-  const entity = Immutable.Record<T>({ id, source, ...fields } as T)
+export function createEntity<T extends IEntity>(
+  source: string,
+  id: string,
+  config: TEntityFields<T>,
+  values?: Partial<T>
+): Immutable.RecordOf<T> {
 
-  entity.prototype.fieldType = fieldType;
+  if (!values) {
+    values = {};
+  }
+
+  const fields: any = {id, source, ...values};
+
+  Object.keys(config)
+    .filter((field: string) => !values[field])
+    .forEach((field: string) => fields[field] = config[field].empty);
+
+  const entity = Immutable.Record<T>(fields as T);
+
+  // entity.prototype.fieldType = fieldType;
 
   return new entity();
 }
